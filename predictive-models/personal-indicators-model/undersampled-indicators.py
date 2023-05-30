d720c99f35cb7596a9a8464648833564344443f3
rawanmahdi <mahdir3@mcmaster.ca>
2023-05-30 10:26:03 -0400

    completed training of undersampled model, really bad accuracy and loss plot

--- predictive-models/personal-indicators-model/undersampled-indicators.py
+++ predictive-models/personal-indicators-model/undersampled-indicators.py
@@ -31,6 +31,9 @@ y_val = val.pop('target')
 X_val = val
 y_test = test.pop('target')
 X_test = test
+print("Training sample:",len(y_train))
+print("Testing sample:", len(y_test))
+print("Validation sample:",len(y_val))
 #%%
 # observe class imbalance
 neg, pos = np.bincount(y_train)
@@ -49,11 +52,18 @@ print("No.negative samples after undersampling",neg0)
 print("No.positive samples after undersampling",pos0)
 
 #%%
+def df_to_dataset(features, labels, batch_size=32, resample=False):
+    tf_dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels)).cache()
+    shuffled_tf_dataset = tf_dataset.shuffle(buffer_size=len(df)) # shuffling values 
+    return shuffled_tf_dataset.batch(batch_size).prefetch(2)# returning 32 samples per batch
+
+#%%
 def get_normalization_layer(feature_name, dataset, batch_size=32):
     # normalize numeric features
     normalizer = layers.Normalization(axis=None)
     # extract feature from dataset
-    feature_data = dataset[feature_name]
+    #feature_data = dataset[feature_name]
+    feature_data = dataset.map(lambda x, y: x[feature_name])
     normalizer.adapt(feature_data, batch_size=batch_size)
     return normalizer
 def get_category_encoding_layer(feature_name, dataset, dtype, max_tokens=None, batch_size=32):
@@ -62,13 +72,19 @@ def get_category_encoding_layer(feature_name, dataset, dtype, max_tokens=None, b
     else:
         index = layers.IntegerLookup(max_tokens=max_tokens)
     # extract feature from dataset
-    feature_ds = dataset[feature_name]
+    #feature_ds = dataset[feature_name]
+    feature_data = dataset.map(lambda x, y: x[feature_name])
     # 'learn' all possible feature values, assign each an int index 
-    index.adapt(feature_ds, batch_size=batch_size)
+    index.adapt(feature_data, batch_size=batch_size)
     # encode integer index
     encoder = layers.CategoryEncoding(num_tokens=index.vocabulary_size(), output_mode="one_hot")
     # multi-hot encode indeices - lambda function captures layers
     return lambda feature: encoder(index(feature))
+#%%
+train_resampled_ds= df_to_dataset(X_train_resampled, y_train_resampled)
+val_ds= df_to_dataset(X_val, y_val)
+test_ds= df_to_dataset(X_test, y_test)
+
 #%%
 inputs = []
 encoded_features =[]
@@ -79,7 +95,7 @@ for header in ["bmi", "physicalHealth", "mentalHealth", 'sleepHours' ]:
     # keras inputs array
     inputs.append(num_col)
 
-    norm_layer = get_normalization_layer(feature_name=header, dataset=X_train_resampled)
+    norm_layer = get_normalization_layer(feature_name=header, dataset=train_resampled_ds)
     encoded_num_col = norm_layer(num_col)
     # encoded feature
     encoded_features.append(encoded_num_col)
@@ -96,10 +112,39 @@ for header in ["smoking","alcoholDrinking","stroke","diffWalk",
 
     # get preprocessing layer 
     cat_layer = get_category_encoding_layer(feature_name=header,
-                                            dataset=X_train_resampled, 
+                                            dataset=train_resampled_ds, 
                                             dtype='string', 
                                             max_tokens=None)
     encoded_cat_col = cat_layer(cat_col)
     # encoded feature
     encoded_features.append(encoded_cat_col)
+# %%
+# KERAS FUNCTIONAL API - MODEL BUILD   
+# merge list feature inputs into one vector
+features = tf.keras.layers.concatenate(encoded_features)
+x = tf.keras.layers.Dense(units=128, activation="relu")(features)
+x = tf.keras.layers.Dropout(rate=0.2)(x)
+x = tf.keras.layers.Dense(units=128, activation='relu')(x)
+output = tf.keras.layers.Dense(units=1, activation='sigmoid')(x)
+model = tf.keras.Model(inputs, output)
+
+#%%
+model.compile(optimizer='adam', 
+              loss='binary_crossentropy', 
+              metrics = ['accuracy'])
+
+result = model.fit(train_resampled_ds,
+                    validation_data=val_ds, 
+                    epochs=50,
+                    verbose=1)
+
+# %%
+plt.plot(result.history['loss'], label='loss')
+plt.plot(result.history['val_loss'], label='val_loss')
+plt.legend()
+#%%
+predictions = model.predict(test_ds)
+binary_predictions = tf.round(predictions).numpy().flatten()
+print(classification_report(y_test, binary_predictions))
+
 # %%
