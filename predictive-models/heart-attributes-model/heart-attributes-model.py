c5b91dd462aa3fa19d2bbf720e4e7d28a7510b83
rawanmahdi <mahdir3@mcmaster.ca>
2023-06-14 10:32:06 -0400

    retrained model on higher quality data

--- predictive-models/heart-attributes-model/heart-attributes-model.py
+++ predictive-models/heart-attributes-model/heart-attributes-model.py
@@ -7,17 +7,53 @@ import matplotlib.pyplot as plt
 # %%
 heart_csv_path = 'C:/Users/Rawan Alamily/Downloads/McSCert Co-op/explainable-ai-heart/predictive-models/heart-attributes-model/data/heart.csv'
 dataframe = pd.read_csv(heart_csv_path)
-dataframe['corrected_target'] = np.where(dataframe['target']==0,1,0)
-dataframe.pop('target')
-dataframe.head()
+dataframe['Sex'] = np.where(dataframe['Sex'] == "M",1,0)
+dataframe['ExerciseAngina'] = np.where(dataframe['ExerciseAngina'] == "Y",1,0)
+def cp(x):
+    if x=='TA':
+        y=1
+    elif x=='ATA':
+        y=0
+    elif x=='NAP':
+        y=2
+    else:
+        y=3
+    return y
+
+def restECG(x):
+    if x=='Normal':
+        y=1
+    elif x=='ST':
+        y=0
+    else:
+        y=2
+    return y
+
+def slope(x):
+    if x=='Up':
+        y=1
+    elif x=='Flat':
+        y=0
+    else:
+        y=2
+    return y
+#%%
+for i in range(dataframe.iloc[:, 2].shape[0]):
+    dataframe.iloc[i,2] = cp(dataframe.iloc[i,2])
+for i in range(dataframe.iloc[:, 6].shape[0]):
+    dataframe.iloc[i,6] = restECG(dataframe.iloc[i,6])
+for i in range(dataframe.iloc[:, 10].shape[0]):
+    dataframe.iloc[i,10] = slope(dataframe.iloc[i,10])
+#%%
+dataframe.head(10)
 #%%
 train, val, test = np.split(dataframe.sample(frac=1), [int(0.5*len(dataframe)), int(0.9*len(dataframe))])
 # train, test = train_test_split(dataframe, test_size=0.2, random_state=42)
-y_train = train.pop('corrected_target')
+y_train = train.pop('HeartDisease')
 X_train = train
-y_val = val.pop('corrected_target')
+y_val = val.pop('HeartDisease')
 X_val = val
-y_test = test.pop('corrected_target')
+y_test = test.pop('HeartDisease')
 X_test = test
 print(len(X_train))
 print(len(X_val))
@@ -33,9 +69,11 @@ X_test_NORM = scaler.transform(X_test)
 #%%
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(units=128, activation='relu'), 
-    tf.keras.layers.Dropout(rate=0.2),
-    tf.keras.layers.Dense(units=128, activation='relu'), 
+    tf.keras.layers.Dense(units=64, activation='relu'), 
+    tf.keras.layers.Dropout(rate=0.7),
+    tf.keras.layers.Dense(units=500, activation='relu'), 
+    tf.keras.layers.Dropout(rate=0.7),
+    tf.keras.layers.Dense(units=64, activation='relu'), 
     tf.keras.layers.Dense(units=1, activation='sigmoid')
 ])
 model.compile(optimizer="adam", 
@@ -43,7 +81,7 @@ model.compile(optimizer="adam",
               metrics=["accuracy"])
 #%%
 result = model.fit(X_train_NORM, y_train, 
-          epochs=50,
+          epochs=100,
           batch_size=32,
           validation_data=(X_val_NORM, y_val))
 
@@ -57,7 +95,7 @@ plt.plot(result.history['val_accuracy'], label='val_accuracy')
 plt.legend()
 model.evaluate(X_test_NORM, y_test) 
 #%%
-binary_predictions = tf.round(model.predict(X_test)).numpy().flatten()
+binary_predictions = tf.round(model.predict(X_test_NORM)).numpy().flatten()
 print(classification_report(y_test, binary_predictions))
 #%%
 model.save("C:/Users/Rawan Alamily/Downloads/McSCert Co-op/explainable-ai-heart/predictive-models/heart-attributes-model/saved-model")
